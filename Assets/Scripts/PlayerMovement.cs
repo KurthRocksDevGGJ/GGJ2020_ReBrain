b8f79e0c76299759b58e980ce2f3e013c1309a25
Marcel Volland <m.volland@ipi-gmbh.com>
2020-02-01 18:31:11 +0100

    Added networking

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Photon.Pun.Demo.PunBasics
+{
+    public class PlayerMovement : MonoBehaviourPunCallbacks
+    {
+        public PlayerController playerController;
+
+        public float moveSpeed;
+
+        private float _horizontalMove = 0f;
+        private bool _jump = false;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (photonView.IsMine)
+            {
+                _horizontalMove = moveSpeed;
+
+                if(Input.GetButtonDown("Jump")) 
+                {
+                    _jump = true;
+                }
+            }
+        }
+
+        void FixedUpdate() {
+            playerController.Move(_horizontalMove * Time.fixedDeltaTime, false, _jump);
+            _jump = false;
+        }
+    }
+}
